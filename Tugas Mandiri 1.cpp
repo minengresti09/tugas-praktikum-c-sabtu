7896f155f447f0721723aa40bd660a1cfe76db9e
unknown <minengresti09@gmail.com>
2024-09-26 23:08:20 +0700

    first commit

---
+++ Tugas Mandiri 1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	char Nama[]= "Mineng Resti Yanuar Virgin";
+	char Jurusan[]= "Teknik Informatika";
+	char Nim[]= "2441027";
+	
+	printf("Nama	:%s\n", Nama);
+	printf("Jurusan	:%s\n", Jurusan);
+	printf("Nim	:%s\n", Nim);
+	
+	return 0;
+}
---
+++ Tugas Mandiri 2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    
+  int panjang = 10;
+  int lebar = 7;
+  int luas = panjang * lebar;
+
+   
+    printf("Luas persegi panjang adalah: %d\n", luas);
+
+    return 0;
+
+
+}
